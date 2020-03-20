387008a717396f9ff640746382e0e2f02656fcde
rufusnufus <rufusnorbert14@gmail.com>
2020-03-20 17:15:24 +0300

    Speed up execution time

--- assignment_1/query5.py
+++ assignment_1/query5.py
@@ -1,6 +1,6 @@
 from dijkstar import Graph, find_path
 from dijkstar.algorithm import NoPathError
-from query2 import actors_to_actors, actors
+from query2 import calculate_matches, set_actors_and_film_actors
 from pymongo import MongoClient
 import csv
 
@@ -10,6 +10,8 @@ con = MongoClient("mongodb://localhost")
 #connecting to database named dvdrental in mongo
 db = con['dvdrental']
 
+actors, film_actors = set_actors_and_film_actors(db)
+actors_to_actors = calculate_matches(actors, film_actors)
 graph = Graph()
 for i in actors:
 	for j in actors:
@@ -18,26 +20,31 @@ for i in actors:
 			graph.add_edge(j, i, 1)
 
 
-actor = "Penelope Guiness"
+print("Input the name and surname of the actor(e.q. Ivan Ivanov):", end = ' ')
+actor = input()
 actor_name, actor_surname = actor.split()
-actor_id = db['actor'].find_one({'first_name': actor_name, 'last_name': actor_surname})['actor_id']
+try:
+	actor_id = db['actor'].find_one({'first_name': actor_name, 'last_name': actor_surname})['actor_id']
 
-with open('query5.csv', 'w', newline='') as f:
-	fieldnames = ['actor id', 'first name', 'last name', 'Bacon number']
+	with open('query5.csv', 'w', newline='') as f:
+		fieldnames = ['actor id', 'first name', 'last name', 'Bacon number']
 
-	writer = csv.DictWriter(f, fieldnames=fieldnames)
-	writer.writeheader()
+		writer = csv.DictWriter(f, fieldnames=fieldnames)
+		writer.writeheader()
 
-	for alt_actor_id in actors:
-		dic = {}
-		dic['actor id'] = alt_actor_id
-		dic['first name'] = db['actor'].find_one({'actor_id': alt_actor_id})['first_name']
-		dic['last name'] = db['actor'].find_one({'actor_id': alt_actor_id})['last_name']
-		try:
-			bacon_num = find_path(graph, actor_id, alt_actor_id).total_cost
-		except NoPathError:
-			bacon_num = None
-		dic['Bacon number'] = bacon_num
-		writer.writerow(dic)
+		for alt_actor_id in actors:
+			dic = {}
+			dic['actor id'] = alt_actor_id
+			dic['first name'] = db['actor'].find_one({'actor_id': alt_actor_id})['first_name']
+			dic['last name'] = db['actor'].find_one({'actor_id': alt_actor_id})['last_name']
+			try:
+				bacon_num = find_path(graph, actor_id, alt_actor_id).total_cost
+			except NoPathError:
+				bacon_num = None
+			dic['Bacon number'] = bacon_num
+			writer.writerow(dic)
+except:
+	print('There is no such actor in the database:( We are sorry.')
+	print('We hope that this actor will become famous soon and will be added into DB.')
 
 con.close()
