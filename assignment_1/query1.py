b1520e3235b177f8a97b8b0a10419c2cf04f1dc8
rufusnufus <rufusnorbert14@gmail.com>
2020-03-20 15:15:39 +0300

    Update adding categories to customer

--- assignment_1/query1.py
+++ assignment_1/query1.py
@@ -6,21 +6,21 @@ con = MongoClient("mongodb://localhost")
 
 #connecting to database named dvdrental in mongo
 db = con['dvdrental']
-rent = db['rental']
 
 def find_recent_year(collection):
 	year_frequency = {entry['rental_date'].year for entry in collection.find()}
 	return(max(year_frequency))
 
-recent_year = find_recent_year(rent)
+recent_year = find_recent_year(db['rental'])
 population = {}
-for entry in rent.find():
+for entry in db['rental'].find():
 	if entry['rental_date'].year == recent_year:
 		if entry['customer_id'] not in population:
 			population[entry['customer_id']] = set()
 		film_id = [i['film_id'] for i in db['inventory'].find({'inventory_id': entry['inventory_id']})]
-		for film in db['film_category'].find({'film_id': film_id[0]}):
-			population[entry['customer_id']].add(film['category_id'])
+		for film in db['film_category'].find({'film_id': film_id[0]},{'_id':0, 'categories':1}):
+			population[entry['customer_id']].add(film['categories'][0]['category'])
+
 file = []
 for customer in population.keys():
 	if len(population[customer]) >= 2:
@@ -33,7 +33,7 @@ with open('query1.csv', 'w', newline='') as f:
 
 	writer.writeheader()
 	for i in file:
-		writer.writerow({'customer_id': i['customer_id'], 'store_id': i['store_id'], 'first_name': i['store_id'],
+		writer.writerow({'customer_id': i['customer_id'], 'store_id': i['store_id'], 'first_name': i['first_name'],
 		 'last_name': i['last_name'], 'email': i['email'], 'address_id':i['address_id'],'activebool':i['activebool'],
 		 'create_date': i['create_date'], 'last_update': i['last_update'], 'active': i['active']})
 
